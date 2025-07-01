9861d538d6d64d5ff51d1d106fc9c12a4648c916
vybhav-ibr <119930636+vybhav-ibr@users.noreply.github.com>
2025-07-01 10:28:22 +0530

    Update ALL_SCRIPT.sh

--- ALL_SCRIPT.sh
+++ ALL_SCRIPT.sh
@@ -17,7 +17,7 @@ add-apt-repository -y universe
 apt-get update -y
 
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
 apt-get update -y
