3974c8871abd78f71ccdf2e4295437096dbd4901
vybhav-ibr <119930636+vybhav-ibr@users.noreply.github.com>
2025-07-01 10:39:02 +0530

    Update ALL_SCRIPT.sh

--- ALL_SCRIPT.sh
+++ ALL_SCRIPT.sh
@@ -3,9 +3,9 @@ set -e
 set -o pipefail
 
 apt-get update -y
-apt-get install -y build-essential software-properties-common curl git git-lfs locales python3-venv python3-pip
+sudo apt-get install -y build-essential software-properties-common curl git git-lfs locales python3-venv python3-pip
 
-apt-get install -y gcc-11 g++-11
+sudo apt-get install -y gcc-11 g++-11
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 200
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 200
 
@@ -20,20 +20,17 @@ export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrast
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
-apt-get update -y
-apt-get install -y ros-dev-tools
-apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get install -y ros-dev-tools
+sudo apt-get upgrade -y
 
-apt-get install -y \
+sudo apt-get install -y \
   ros-jazzy-desktop \
   python3-colcon-common-extensions \
   ros-jazzy-xacro \
   ros-jazzy-ros2-control \
   ros-jazzy-ros2-controllers \
   ros-jazzy-joint-state-publisher \
-  ros-jazzy-ros-gz \
-  ros-jazzy-gz-ros2-control \
-  ros-jazzy-gz-ros-pkgs
 
 git clone https://github.com/isaac-sim/IsaacSim.git isaacsim
 cd isaacsim
